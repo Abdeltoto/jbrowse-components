ddf6b3998b3f29d982c506ba82cb0cd40182317e
Colin <colin.diesh@gmail.com>
2026-03-25 00:35:39 -0400

    Github links and source code

--- products/jbrowse-react-app/stories/JBrowseReactApp.stories.tsx
+++ products/jbrowse-react-app/stories/JBrowseReactApp.stories.tsx
@@ -1,4 +1,13 @@
-export {
+import BasicExampleSource from '!!./raw-source-loader.cjs!./examples/BasicExample.tsx'
+import DarkThemeSource from '!!./raw-source-loader.cjs!./examples/DarkTheme.tsx'
+import HumanDemoSource from '!!./raw-source-loader.cjs!./examples/HumanDemo.tsx'
+import SyntenyExampleSource from '!!./raw-source-loader.cjs!./examples/SyntenyExample.tsx'
+import WithFetchConfigJsonSource from '!!./raw-source-loader.cjs!./examples/WithFetchConfigJson.tsx'
+import WithImportConfigJsonSource from '!!./raw-source-loader.cjs!./examples/WithImportConfigJson.tsx'
+import WithLaunchLinearGenomeViewSource from '!!./raw-source-loader.cjs!./examples/WithLaunchLinearGenomeView.tsx'
+import WithWebWorkerSource from '!!./raw-source-loader.cjs!./examples/WithWebWorker.tsx'
+
+import {
   BasicExample,
   DarkTheme,
   HumanDemo,
@@ -8,5 +17,37 @@ export {
   WithLaunchLinearGenomeView,
   WithWebWorker,
 } from './examples/index.ts'
+
+const sourceMap = [
+  [BasicExample, BasicExampleSource],
+  [DarkTheme, DarkThemeSource],
+  [HumanDemo, HumanDemoSource],
+  [SyntenyExample, SyntenyExampleSource],
+  [WithFetchConfigJson, WithFetchConfigJsonSource],
+  [WithImportConfigJson, WithImportConfigJsonSource],
+  [WithLaunchLinearGenomeView, WithLaunchLinearGenomeViewSource],
+  [WithWebWorker, WithWebWorkerSource],
+] as const
+
+for (const [story, sourceCode] of sourceMap) {
+  const fileName = `${story.name}.tsx`
+  Object.assign(story, {
+    parameters: {
+      storyFile: fileName,
+      docs: { source: { code: sourceCode, language: 'tsx' } },
+    },
+  })
+}
+
+export {
+  BasicExample,
+  DarkTheme,
+  HumanDemo,
+  SyntenyExample,
+  WithFetchConfigJson,
+  WithImportConfigJson,
+  WithLaunchLinearGenomeView,
+  WithWebWorker,
+}
 
 export default { title: 'Source code for examples' }
